875edec7e0efaf0f819cb3607b863e3fa93f5e52
Vivekachooz <vivekachooz@gmail.com>
2020-04-13 21:32:56 +0530

    X00T: Add more overlay support
    Signed-off-by: Vivekachooz <vivekachooz@gmail.com>

--- device.mk
+++ device.mk
@@ -347,7 +347,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-evo
 
 # Power
 PRODUCT_PACKAGES += \
