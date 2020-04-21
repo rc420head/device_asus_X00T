079200c7a0704dbb8eaf76c5c98d5424f227c04b
Vivekachooz <vivekachooz@gmail.com>
2020-04-21 21:01:43 +0530

    X00T: Rename to overlay-evolution
    Signed-off-by: Vivekachooz <vivekachooz@gmail.com>

--- device.mk
+++ device.mk
@@ -352,7 +352,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evo
+    $(LOCAL_PATH)/overlay-evolution
 
 # Power
 PRODUCT_PACKAGES += \
